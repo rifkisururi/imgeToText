3376df04decbc900e7d1d0771b6631f7ac4af389
Rifki Ahmad Sururi <rifkisururi27@gmail.com>
2024-02-12 20:34:20 +0700

    fix body post data

--- app.js
+++ app.js
@@ -1,27 +1,19 @@
-// import { createWorker } from 'tesseract.js';
-
-// (async () => {
-//   const worker = await createWorker('eng');
-//   const ret = await worker.recognize('https://tesseract.projectnaptha.com/img/eng_bw.png');
-//   console.log(ret.data.text);
-//   await worker.terminate();
-// })();
-
 // Import required modules
 import express from 'express';
 import { createWorker } from 'tesseract.js';
+import bodyParser from 'body-parser';
 
 // Create an Express application
 const app = express();
 const port = 3000;
-app.use(express.json());
+
+// Middleware to parse JSON and URL-encoded bodies
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // Define a route for OCR processing
 app.post('/ocr', async (req, res) => {
   try {
-    // Create a Tesseract worker
-    const worker = await createWorker('eng');
-    // console.log(req);
     console.log(req.body);
     const imageUrl = req.body.urlImage;
     console.log('imageUrl ', imageUrl);
@@ -39,21 +31,17 @@ app.post('/ocr', async (req, res) => {
     if(imageUrl64 == undefined){
         res.send("image not found");
     }else{
-        // imageUrl64 = "data:image/jpeg;base64,"+imageUrl64;
         strtImg = imageUrl64;
     }
-
     console.log('strtImg ', strtImg);
+
+    const worker = await createWorker('eng');
     const ret = await worker.recognize(strtImg);
+    await worker.terminate();
     console.log(ret.data.text);
     await worker.terminate();
-    // Send the extracted text as the HTTP response
     res.send(ret.data.text);
-
-    // Terminate the worker after processing
-    await worker.terminate();
   } catch (error) {
-    // Handle errors
     console.error('Error extracting text:', error.message);
     res.status(500).send('Error extracting text');
   }
